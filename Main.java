23f872958e2f9088249a7fd2b026880a58c0c3e4
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-10-08 11:19:35 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
-
 public class Main
 {
   public static void main(String[] args)
   {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the radius of the circle");
+    double radius = input.nextDouble();
+    Circle c1 = new Circle(radius);
+    double area = c1.getArea();
+    double circumference = c1.getCircumference();
+    System.out.println("A circle with a radius of " + radius + " has a circumference of " + circumference + " and an area of " + area);
     // write solutions to problems here
   }
 }
